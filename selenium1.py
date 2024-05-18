c6d3468000ffb40cae00c186282085c9d66386c4
Raj Ojha <Raj.ojha@xyz.com>
2024-05-18 11:17:28 +0530

    first commit

--- selenium1.py
+++ selenium1.py
@@ -13,7 +13,7 @@ class TestFlaskApp(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.implicitly_wait(10)
-        self.driver.get("http://54.221.182.47:5000/")
+        self.driver.get("http://172.17.0.2:5000")
 
     def tearDown(self):
         self.driver.quit()
